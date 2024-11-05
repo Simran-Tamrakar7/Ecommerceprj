788ddaa43568ec2a75f03af6d5a856c77255006a
Simran-Tamrakar7 <simrantamrakar77@gmail.com>
2024-11-05 18:30:43 +0545

    jazzmin installed and index forntend done

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
-
+from django.http import HttpResponse 
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Welcome to Our shop")
+    return render(request, 'core/index.html')
