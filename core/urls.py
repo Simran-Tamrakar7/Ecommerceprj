f713e17b1271bb04dae8e3156742a40317fd281c
Simran-Tamrakar7 <simrantamrakar77@gmail.com>
2024-11-08 10:48:11 +0545

    product,categories and vendor

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
-from core.views import index
+from core.views import index, product_list_view, category_list_view,category_product_list__view, vendor_list_view,vendor_detail_view
 # from core import *
 
 app_name = "core"
 
 urlpatterns = [
+
+    # Homepage
     path("", index, name="index"),
+    path("products/", product_list_view, name="product-list"),
+
+    # category
+    path("category/", category_list_view, name="category-list"),
+    path("category/<cid>/", category_product_list__view, name="category-product-list"),
+
+    # Vendor
+    path("vendors/", vendor_list_view, name="vendor-list"),
+    path("vendors/<vid>/", vendor_detail_view, name="vendor-detail"),
+    
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,68 @@
 from django.http import HttpResponse 
 from django.shortcuts import render
+from django.db.models import Count
+
+from core.models import Product,Category,Vendor, CartOrder, CartOrderItems,ProductImages, ProductReview, wishlist, Address
+
 
 def index(request):
-    return render(request, 'core/index.html')
+    # products = Product.objects.all().order_by("-id") vdo14
+    products = Product.objects.filter(product_status="published",featured=True).order_by("-id")
+
+    context = {
+        "products": products
+    }
+
+    return render(request, 'core/index.html', context)
+
+
+def product_list_view(request):
+    products = Product.objects.filter(product_status="published",).order_by("-id")
+
+    context = {
+        "products": products
+    }
+
+    return render(request, 'core/product-list.html', context)
+
+
+def category_list_view(request):
+    categories = Category.objects.all().order_by("-id")
+    # categories = Category.objects.all().annotate(product_count=Count("product"))
+
+    context = {
+        "categories": categories
+    }
+    return render(request,'core/category-list.html',context)
+    
+def category_product_list__view(request,cid):
+
+    category = Category.objects.get(cid=cid)
+    products=Product.objects.filter(product_status="published",category=category)
+
+    context = {
+        "category": category,
+        "products":products,
+    }
+    return render(request,"core/category-product-list.html",context)
+
+
+def vendor_list_view(request):
+    vendors= Vendor.objects.all()
+    context = {
+        "vendors": vendors
+    }
+    return render(request, 'core/vendor-list.html', context)
+
+
+def vendor_detail_view(request, vid):
+    vendor= Vendor.objects.get(vid=vid)
+    products = Product.objects.filter(vendor=vendor, product_status="published")
+
+    context = {
+        "vendor": vendor,
+        "products": products,
+    }
+    return render(request, 'core/vendor-detail.html', context)
+
+
