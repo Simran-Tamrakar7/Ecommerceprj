16cc2712aa95136f79220dec016b813b4ad2f938
Simran-Tamrakar7 <simrantamrakar77@gmail.com>
2024-11-10 12:08:33 +0545

    Add to cart

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from core.views import index, product_list_view, category_list_view,category_product_list__view, vendor_list_view,vendor_detail_view, product_detail_view, tag_list,ajax_add_review,search_view,filter_product
+from core.views import index, product_list_view, category_list_view,category_product_list__view, vendor_list_view,vendor_detail_view, product_detail_view, tag_list,ajax_add_review,search_view,filter_product,add_to_cart
 # from core import *
 
 app_name = "core"
@@ -28,5 +28,8 @@ urlpatterns = [
     # Homepage
     path("search/", search_view, name="search"),
 
+    # Filter product URL
     path("filter-products/", filter_product, name="filter-product"),
+
+    path("add-to-cart/", add_to_cart, name="add-to-cart"),
 ]
--- static/assets/js/function.js
+++ static/assets/js/function.js
@@ -61,7 +61,6 @@ $("#commentForm").submit(function (e) {
 })
 
 
-
 $(document).ready(function () {
     $(".filter-checkbox, #price-filter-btn").on("click", function () {
         console.log("A checkbox have been clicked");
@@ -132,5 +131,107 @@ $(document).ready(function () {
         }
 
     })
+// })
 
+    $(".add-to-cart-btn").on("click", function () {
+
+        let this_val = $(this)
+        let index = this_val.attr("data-index")
+
+        let quantity = $(".product-quantity-" + index).val()
+        let product_title = $(".product-title-" + index).val()
+    
+        let product_id = $(".product-id-" + index).val()
+        let product_price = $("#current-product-price-" + index).text()
+    
+        let product_pid = $(".product-pid-" + index).val()
+        let product_image = $(".product-image-" + index).val()
+
+
+        console.log("Quantity:", quantity);
+        console.log("Title:", product_title);
+        console.log("Price:", product_price);
+        console.log("ID:", product_id);
+        console.log("PID:", product_pid);
+        console.log("Image:", product_image);
+        console.log("Index:", index);
+        console.log("Currrent Element:", this_val);
+
+        $.ajax({
+            url: '/add-to-cart',
+            data: {
+                'id': product_id,
+                'pid': product_pid,
+                'image': product_image,
+                'qty': quantity,
+                'title': product_title,
+                'price': product_price,
+            },
+            dataType: 'json',
+            beforeSend: function () {
+                console.log("Adding Product to Cart...");
+            },
+            success: function (response) {
+                this_val.html("Item added to cart ✓")
+                // this_val.html("<i class='fas fa-check-circle'></i>")
+
+                console.log("Added Product to Cart!");
+                $(".cart-items-count").text(response.totalcartitems)
+
+
+            }
+        })
+    })
 })
+
+
+
+// Add to cart functionality
+//     $(".add-to-cart-btn").on("click", function() {
+//         let quantity = $("#product-quantity" ).val()
+//         let product_title = $(".product-title").val()
+//         let product_id = $(".product-id").val()
+//         let product_price = $("#current-product-price" ).text()
+//         let this_val = $(this)
+//         // let index = this_val.attr("data-index")
+
+
+//     //     let product_pid = $(".product-pid-" + index).val()
+//     //     let product_image = $(".product-image-" + index).val()
+
+
+//         console.log("Quantity:", quantity);
+//         console.log("Title:", product_title);
+//         console.log("Price:", product_price);
+//         console.log("ID:", product_id);
+//     //     console.log("PID:", product_pid);
+//     //     console.log("Image:", product_image);
+//     //     console.log("Index:", index);
+//         console.log("Currrent Element:", this_val);
+
+//         $.ajax({
+//             url: '/add-to-cart',
+//             data: {
+//                 'id': product_id,
+//     //             'pid': product_pid,
+//     //             'image': product_image,
+//                 'qty': quantity,
+//                 'title': product_title,
+//                 'price': product_price,
+//             },
+//             dataType: 'json',
+//             beforeSend: function () {
+//                 console.log("Adding Product to Cart...");
+//             },
+//             success: function (response) {
+//                 this_val.html("Item added to cart ✓")
+//     //             this_val.html("<i class='fas fa-check-circle'></i>")
+
+//                 console.log("Added Product to Cart!");
+//                 $(".cart-items-count").text(response.totalcartitems)
+
+
+//             }
+//         })
+//     })
+// })
